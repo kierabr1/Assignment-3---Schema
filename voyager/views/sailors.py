f6b7c190342d579695e74b54585be0aa6b75baa0
Kiera Brown <klbrown@klbrown-macbookpro.roam.corp.google.com>
2020-02-26 15:53:00 -0800

    Added functionality for list of sailors who've sailed a boat and list of boats that a sailor has sailed.

--- voyager/views/sailors.py
+++ voyager/views/sailors.py
@@ -12,6 +12,12 @@ from voyager.validate import NAME_RE, INT_RE, DATE_RE
 def sailors(conn):
     return execute(conn, "SELECT s.sid, s.name, s.age, s.experience FROM Sailors AS s")
 
+def sailor_who(conn, boat_n):
+    return execute(conn, f"SELECT s.sid, s.name FROM Sailors AS s INNER JOIN Voyages ON s.sid=Voyages.sid INNER JOIN Boats ON Boats.bid=Voyages.bid WHERE Boats.name='{boat_n}'")
+
+def boats_who(conn, sailors_n):
+    return execute(conn, f"SELECT b.bid, b.name FROM Boats AS b INNER JOIN Voyages ON b.bid=Voyages.bid INNER JOIN Sailors ON Sailors.sid=Voyages.sid WHERE Sailors.name='{sailors_n}'")
+
 def views(bp):
     @bp.route("/sailors")
     def _get_all_sailors():
@@ -20,5 +26,21 @@ def views(bp):
         return render_template("table.html", name="sailors", rows=rows)
 
 
+    @bp.route("/sailors/who-sailed", methods = ["POST"])
+    def _datainput1():
+        if request.method == "POST":
+            boat_n = request.form['boat-name']
+            with get_db() as conn:
+                rows = sailor_who(conn, boat_n)
+            return render_template("table.html", rows=rows, name="Boats")
+
+
+    @bp.route("/boats/sailed-by", methods = ["POST"])
+    def _datainput2():
+        if request.method == "POST":
+            sailors_n = request.form['sailor-name']
+            with get_db() as conn:
+                rows = boats_who(conn, sailors_n)
+            return render_template("table.html", rows=rows, name="Sailors")
 
 
