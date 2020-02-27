e3369e9605714b3147f774d8af2745bbc9e3fc7b
Kiera Brown <klbrown@klbrown-macbookpro.roam.corp.google.com>
2020-02-26 20:12:47 -0800

    Added in sailors.py- sailors who sailed on date

--- voyager/views/sailors.py
+++ voyager/views/sailors.py
@@ -18,6 +18,9 @@ def sailor_who(conn, boat_n):
 def boats_who(conn, sailors_n):
     return execute(conn, f"SELECT b.bid, b.name FROM Boats AS b INNER JOIN Voyages ON b.bid=Voyages.bid INNER JOIN Sailors ON Sailors.sid=Voyages.sid WHERE Sailors.name='{sailors_n}'")
 
+def dates_who(conn, s_date):
+    return execute(conn, f"SELECT s.sid, s.name FROM Sailors AS s INNER JOIN Voyages ON s.sid=Voyages.sid WHERE Voyages.date_of_voyage='{s_date}'")
+
 def views(bp):
     @bp.route("/sailors")
     def _get_all_sailors():
@@ -41,6 +44,14 @@ def views(bp):
             sailors_n = request.form['sailor-name']
             with get_db() as conn:
                 rows = boats_who(conn, sailors_n)
+            return render_template("table.html", rows=rows, name="Sailors")
+
+    @bp.route("/sailors/who-sailed-on-date", methods = ["POST"])
+    def _datainput3():
+        if request.method == "POST":
+            s_date = request.form['date']
+            with get_db() as conn:
+                rows = dates_who(conn, s_date)
             return render_template("table.html", rows=rows, name="Sailors")
 
 
