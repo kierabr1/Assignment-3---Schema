d0df22d547cbaf7eb3bee692c30c7631c4bd43b4
Kiera Brown <klbrown@klbrown-macbookpro.roam.corp.google.com>
2020-02-27 23:32:57 -0800

    Add sailor, boat, and voyage feature complete

--- voyager/views/sailors.py
+++ voyager/views/sailors.py
@@ -4,6 +4,7 @@ from flask import g
 from flask import escape
 from flask import render_template
 from flask import request
+from flask import Flask,redirect
 
 from voyager.db import get_db, execute
 from voyager.validate import validate_field, render_errors
@@ -24,8 +25,47 @@ def dates_who(conn, s_date):
 def colors_who(conn, s_color):
     return execute(conn, f"SELECT s.sid, s.name FROM Sailors AS s INNER JOIN Voyages ON s.sid=Voyages.sid INNER JOIN Boats ON Boats.bid=Voyages.bid WHERE Boats.color='{s_color}'")
 
+def add_sailor(conn, add_s_name,add_s_age,add_s_exp):
+    return execute(conn, f"INSERT INTO Sailors(name, age, experience) VALUES ('{add_s_name}','{add_s_age}','{add_s_exp}')")
+
+def add_boat(conn, add_b_name,add_b_color):
+    return execute(conn, f"INSERT INTO Boats(name, color) VALUES ('{add_b_name}','{add_b_color}')")
+
+def add_voyage(conn, add_v_sid,add_v_bid, add_v_date):
+    return execute(conn, f"INSERT INTO Voyages(sid, bid, date_of_voyage) VALUES ('{add_v_sid}','{add_v_bid}','{add_v_date}')")
+
 
 def views(bp):
+    @bp.route("/sailors/add", methods=["POST"])
+    def _add_sailor():
+        if request.method == "POST":
+            add_s_name = request.form['add-sailor-name']
+            add_s_age = request.form['add-sailor-age']
+            add_s_exp = request.form['add-sailor-experience']
+            with get_db() as conn:
+                add_sailor(conn, add_s_name, add_s_age, add_s_exp)
+            return redirect("/sailors")
+
+    @bp.route("/boats/add", methods=["POST"])
+    def _add_boat():
+        if request.method == "POST":
+            add_b_name = request.form['add-boat-name']
+            add_b_color = request.form['add-boat-color']
+            with get_db() as conn:
+                add_boat(conn, add_b_name, add_b_color)
+            return redirect("/boats")
+
+    @bp.route("/voyages/add", methods=["POST"])
+    def _add_voyage():
+        if request.method == "POST":
+            add_v_sid = request.form['add-voyage-sid']
+            add_v_bid = request.form['add-voyage-bid']
+            add_v_date = request.form['add-voyage-date']
+            with get_db() as conn:
+                add_voyage(conn, add_v_sid, add_v_bid, add_v_date)
+            return redirect("/voyages")
+
+
     @bp.route("/sailors")
     def _get_all_sailors():
         with get_db() as conn:
